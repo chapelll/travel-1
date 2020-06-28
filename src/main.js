53269a16bbbb5829ca977ba579ee34f34458bab0
chapelll <214426857@qq.com>
2020-06-28 17:26:33 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastclick from 'fastclick';
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+
 
 Vue.config.productionTip = false
+fastclick.attach(document.body);
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
+//路由就是根据网址的不同，返回不同的内容给用户
