e906a37aaa1c289a3031f0c6ef7c006680457a0b
chapelll <214426857@qq.com>
2020-07-06 18:42:54 +0800

    使用ajax获取数据

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wrapper">
-    <swiper ref="mySwiper" :options="swiperOptions">
-        <swiper-slide v-for="item of swiperList" :key="item.id"> 
+    <swiper ref="mySwiper" :options="swiperOptions" v-if="showSwiper">
+        <swiper-slide v-for="item of list" :key="item.id"> 
             <img class="swiper-img" :src="item.imgUrl">
         </swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
@@ -12,6 +12,9 @@
 <script>
 export default {
     name: 'HomeSwiper',
+    props: {
+        list: Array
+    },
     data () {
         return {
             swiperOptions: {
@@ -23,18 +26,14 @@ export default {
                 //自动播放间隔时间
                 // speed: 1000,
                 //滑动耗费时间
+               
             },
-            swiperList: [{
-                id: '0001',
-                imgUrl:"//imgs.qunarzz.com/piao/fusion/1802/e3/62ce7362ca051d02.jpg_640x200_6db551b7.jpg" 
-            },{
-                id: '0002',
-                imgUrl: "//imgs.qunarzz.com/vc/e3/a1/71/f498dfd3bed948d623c9093252.jpg_92.jpg"
-            },{
-                id: '0003',
-                imgUrl: "//imgs.qunarzz.com/vc/44/e9/86/95bc36c9e1c06ebd68bdfe222e.jpg_92.jpg"
-            }]
         }
+    },
+    computed: {
+       showSwiper () {
+           return this.list.length
+       } 
     }
 }
 </script>
--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -2,7 +2,7 @@
     <div>
     <div class="title">周末去哪儿</div>
     <ul>
-        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <li class="item border-bottom" v-for="item of list" :key="item.id">
             <div class="item-img-wrapper">
                 <img class="item-img" :src="item.imgUrl" alt="">
             </div>
@@ -18,24 +18,12 @@
 <script>
 export default {
     name: 'HomeWeekend',
+    props: {
+        list: Array
+    },
     data() {
         return {
-            recommendList: [{
-            id: '0001',
-            imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-            title: '大连圣亚海洋世界',
-            desc: '浪漫大连首站，浪漫的海洋主题乐园'
-        },{
-            id: '0002',
-            imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-            title: '大连圣亚海洋世界',
-            desc: '浪漫大连首站，浪漫的海洋主题乐园'
-        },{
-            id: '0003',
-            imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-            title: '大连圣亚海洋世界',
-            desc: '浪漫大连首站，浪漫的海洋主题乐园'
-        },]
+            
         }
     }
 }
@@ -47,11 +35,10 @@ export default {
         line-height .4rem
         background #eee
         text-indent .2rem
-        margin-top .2rem
     .item-img-wrapper
         overflow hidden
         height 0
-        padding-bottom 33.9%
+        padding-bottom 37.09%
         .item-img
             width 100%
     .item-info
