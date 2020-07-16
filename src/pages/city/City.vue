a40a23117e47bc526b14786bec1bed68788f8bdf
chapelll <214426857@qq.com>
2020-07-16 17:40:03 +0800

    使用ajax获取城市信息

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -4,14 +4,15 @@
     </city-header>
     <city-search>
     </city-search>
-    <city-list>
+    <city-list :cities="cities" :hot="hotCities">
     </city-list>
-    <city-alphabet>
+    <city-alphabet :cities="cities">
     </city-alphabet>
 </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header.vue'
 import CitySearch from './components/Search.vue'
 import CityList from './components/List.vue'
@@ -23,6 +24,29 @@ export default {
         CitySearch,
         CityList,
         CityAlphabet,
+    },
+    data () {
+        return {
+            cities: {},
+            hotCities: []
+        }
+    },
+    methods: {
+        getCityInfo() {
+            axios.get("/api/city.json")
+                .then(this.handleGetCityInfoSucc)
+        },
+        handleGetCityInfoSucc (res) {
+            res = res.data
+            if (res.ret && res.data) {
+                let data = res.data
+                this.cities = data.cities
+                this.hotCities = data.hotCities
+            }
+        }
+    },
+    mounted () {
+        this.getCityInfo() 
     }
 }
 </script>
