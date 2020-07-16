38a26ef03f884e3381095bab3f4eb8f394e766bc
chapelll <214426857@qq.com>
2020-07-16 16:09:14 +0800

    完成城市页面布局

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'CityAlphabet',
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
+.list 
+    display flex
+    flex-direction column  //主轴为垂直方向
+    justify-content center //在主轴上以中间对齐
+    position absolute
+    right 0
+    top 1.58rem
+    bottom 0
+    width .4rem
+    .item
+        line-height .4rem
+        text-align center
+        color $bgColor
+</style>
